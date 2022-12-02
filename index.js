2f4f50b1b073643c7b9e9bd974aeba7b70502e9b
SD-IITKGP <jaihindsourabh@kgpian.iitkgp.ac.in>
2022-12-02 19:24:18 +0530

    made heroku server compatile

--- index.js
+++ index.js
@@ -43,6 +43,6 @@ app.post("/", function (req, res) {
 app.post("/faliure", function (req, res) {
   res.sendFile(__dirname + "/signup.html");
 });
-app.listen(3000, function () {
+app.listen(process.env.PORT||3000, function () {
   console.log("server running at port 3000");
 });
